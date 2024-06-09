dcb16f2f459e18aa6e3bcdd96d7f73865996ab8d
Ali Esmaeili <fromiran98@gmail.com>
2024-06-10 00:48:58 +0330

    feat: initialize article application

---
+++ src/article/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth import get_user_model
+
+from shared.models import BaseModel
+
+
+class Article(BaseModel):
+    """
+    Article model
+    --------------------------------
+    title: str - max-256
+    description: text
+    --------------------------------
+    """
+
+    title = models.CharField(max_length=256, default="Default Title")
+    description = models.TextField(default="Default Description")
+
+
+class Point(BaseModel):
+    """
+    Point model
+    --------------------------------
+    article: Foreign key
+    user: Foreign key
+    point: Uint8
+    --------------------------------
+    """
+
+    class Meta:
+        unique_together = ["article", "user"]
+
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    point = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
